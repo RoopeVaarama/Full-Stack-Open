c2b62eea3dcf2e59bfd0a83416fe1226de867040
Roope.Vaarama <Roope.Vaarama@gmail.com>
2023-07-25 13:10:52 +0300

    .

--- Osa1/anekdootit/src/App.js
+++ Osa1/anekdootit/src/App.js
@@ -1,18 +1,42 @@
 import React, { useState } from 'react'
 
-const anecdotes = [
-    'If it hurts, do it more often',
-    'Adding manpower to a late software project makes it later!',
-    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-    'Premature optimization is the root of all evil.',
-    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-]
-const points = [0, 0, 0, 0, 0, 0];
-const copy = [...points];
+const App = () => {
+    const anecdotes = [
+        'If it hurts, do it more often',
+        'Adding manpower to a late software project makes it later!',
+        'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+        'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+        'Premature optimization is the root of all evil.',
+        'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+    ]
+    const points = [0, 0, 0, 0, 0, 0];
+
+    const [selected, setSelected] = useState(0);
+    const [copy, setCopy] = useState([...points]);
 
 
-const Anecdote = ({ selected }) => {
+    let randomNumber = [Math.floor(Math.random() * anecdotes.length)];
+    console.log(randomNumber);
+    return (
+        <div>
+            <h1>Anecdote of the day</h1>
+            <Anecdote selected={selected} anecdotes={anecdotes} />
+            <Vote copy={copy} selected={selected} />
+            <button onClick={() => {
+                const copy2 = [...copy]
+                copy2[selected] += 1
+                setCopy(copy2)
+            }}>Vote</button>
+            <button onClick={() => setSelected(randomNumber)}>Next anecdote</button>
+            <TopVoted copy={copy} anecdotes={anecdotes} />
+        </div>
+    )
+}
+
+
+
+
+const Anecdote = ({ anecdotes, selected }) => {
     return (
         <div>
             <p>{anecdotes[selected]}</p>
@@ -45,10 +69,11 @@ function indexOfMax(arr) {
     return maxIndex;
 
 }
-const TopVoted = ({ copy }) => {
+const TopVoted = ({ anecdotes, copy }) => {
+    console.log(copy);
     var high = indexOfMax(copy);
     console.log(high)
-    if (high => 0) {
+    if (copy[high] > 0) {
         return (
             <div>
                 <h1>Anecdote with most votes</h1>
@@ -66,20 +91,4 @@ const TopVoted = ({ copy }) => {
         )
     }
 }
-const App = (props) => {
-    const [selected, setSelected] = useState(0)
-    let randomNumber = [Math.floor(Math.random() * anecdotes.length)];
-    console.log(randomNumber);
-    return (
-        <div>
-            <h1>Anecdote of the day</h1>
-            <Anecdote selected={selected} />
-            <Vote copy={copy} selected={selected} />
-            <button onClick={() => copy[selected] += 1}>Vote</button>
-            <button onClick={() => setSelected(randomNumber)}>Next anecdote</button>
-            <TopVoted copy={copy} />
-        </div>
-    )
-}
-
 export default App;
